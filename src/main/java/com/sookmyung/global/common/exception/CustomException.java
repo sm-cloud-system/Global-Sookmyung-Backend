65386bbac15c9af237452cdd3c5e5f238e3eff66
sung-silver <jinna0319@gmail.com>
2024-11-14 18:14:49 +0900

    [FEAT] common에 exception 패키지 구현

---
+++ src/main/java/com/sookmyung/global/common/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.sookmyung.global.common.exception;
+
+import com.sookmyung.global.common.code.*;
+
+import lombok.*;
+
+@Getter
+public class CustomException extends RuntimeException {
+  private final ExceptionCode exceptionCode;
+
+  public CustomException(ExceptionCode exceptionCode) {
+    super(exceptionCode.message());
+    this.exceptionCode = exceptionCode;
+  }
+
+  public int getHttpStatus() {
+    return exceptionCode.status().value();
+  }
+}
