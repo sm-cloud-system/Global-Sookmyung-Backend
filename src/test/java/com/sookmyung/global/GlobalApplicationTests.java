134582006bd426d82f994200222805e81d95806d
sung-silver <jinna0319@gmail.com>
2024-12-12 23:22:52 +0900

    [TEST] actvateProfiles 설정

--- src/test/java/com/sookmyung/global/GlobalApplicationTests.java
+++ src/test/java/com/sookmyung/global/GlobalApplicationTests.java
@@ -2,8 +2,10 @@ package com.sookmyung.global;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GlobalApplicationTests {
 
   @Test
