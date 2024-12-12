347c3dd41c92534b85b6fd595dbf9b97483b136f
sung-silver <jinna0319@gmail.com>
2024-12-12 14:39:04 +0900

    [FEAT] Member 엔티티 구현

---
+++ src/main/java/com/sookmyung/global/domain/member/entity/Member.java
@@ -0,0 +1,33 @@
+package com.sookmyung.global.domain.member.entity;
+
+import java.time.*;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String firstName;
+
+  @Column(nullable = false)
+  private String lastName;
+
+  @Column(nullable = false)
+  private LocalDate birthDate;
+
+  @Column(nullable = false)
+  private String nickname;
+}
