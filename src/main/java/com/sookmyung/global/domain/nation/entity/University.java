b4bd5b7eb43f12b5cc66749875106c09e4ac1414
sung-silver <jinna0319@gmail.com>
2024-12-12 14:46:49 +0900

    [FEAT] University 엔티티 구현

---
+++ src/main/java/com/sookmyung/global/domain/nation/entity/University.java
@@ -0,0 +1,23 @@
+package com.sookmyung.global.domain.nation.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class University {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "nation_id")
+  private Nation nation;
+}
