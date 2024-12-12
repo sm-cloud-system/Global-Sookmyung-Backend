5a11275053c41a213491f24d37705dced2758c3a
sung-silver <jinna0319@gmail.com>
2024-12-12 22:25:47 +0900

    [FEAT] Role Enum 구현

---
+++ src/main/java/com/sookmyung/global/common/enums/Role.java
@@ -0,0 +1,12 @@
+package com.sookmyung.global.common.enums;
+
+public enum Role {
+  /**
+   * @note: GUEST 이메일 인증을 성공한 유저
+   * @note: SOOKMYUNG_STUDENT 숙명여자대학교 재학생
+   * @note: INTERNATIONAL_STUDENT 유학생
+   */
+  ADMIN,
+  SOOKMYUNG_STUDENT,
+  INTERNATIONAL_STUDENT
+}
