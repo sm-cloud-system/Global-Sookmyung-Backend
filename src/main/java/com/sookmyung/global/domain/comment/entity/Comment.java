f0c66fb4506dd74692b60658ba807e1489f749f8
sung-silver <jinna0319@gmail.com>
2024-12-12 16:22:40 +0900

    [FEAT] Comment 엔티티 구현

---
+++ src/main/java/com/sookmyung/global/domain/comment/entity/Comment.java
@@ -0,0 +1,25 @@
+package com.sookmyung.global.domain.comment.entity;
+
+import jakarta.persistence.*;
+
+import com.sookmyung.global.common.base.*;
+import com.sookmyung.global.domain.member.entity.*;
+import com.sookmyung.global.domain.post.entity.*;
+
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTime {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false)
+  private Post post;
+}
