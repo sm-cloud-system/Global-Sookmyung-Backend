e17f9ad5ae87f326cb63c0eafb60cf11b04fc4fd
sung-silver <jinna0319@gmail.com>
2024-12-13 13:50:37 +0900

    [REFACTOR] static import로 변경

--- src/main/java/com/sookmyung/global/domain/auth/controller/AuthController.java
+++ src/main/java/com/sookmyung/global/domain/auth/controller/AuthController.java
@@ -1,5 +1,8 @@
 package com.sookmyung.global.domain.auth.controller;
 
+import static com.sookmyung.global.domain.auth.code.AuthSuccessCode.SUCCESS_CREATE_AND_SEND_EMAIL_CODE;
+import static com.sookmyung.global.domain.auth.code.AuthSuccessCode.SUCCESS_VALIDATE_EMAIL_CODE;
+
 import jakarta.validation.*;
 
 import org.springframework.http.*;
@@ -7,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 
 import com.sookmyung.global.common.response.*;
 import com.sookmyung.global.common.util.*;
-import com.sookmyung.global.domain.auth.code.*;
 import com.sookmyung.global.domain.auth.dto.request.*;
 import com.sookmyung.global.domain.auth.dto.response.*;
 import com.sookmyung.global.domain.auth.service.*;
@@ -25,7 +27,7 @@ public class AuthController implements AuthApi {
   public ResponseEntity<ResponseTemplate> createEmailCode(
       @RequestBody @Valid IssueEmailCodeRequest request) {
     authEmailService.createEmailCode(request);
-    return ResponseUtil.success(AuthSuccessCode.SUCCESS_CREATE_AND_SEND_EMAIL_CODE);
+    return ResponseUtil.success(SUCCESS_CREATE_AND_SEND_EMAIL_CODE);
   }
 
   @RequestMapping("/verify/email")
@@ -33,7 +35,7 @@ public class AuthController implements AuthApi {
   public ResponseEntity<ResponseTemplate<?>> validateEmailCode(
       @RequestBody @Valid EmailVerificationRequest request) {
     IssueTokenForGuestResponse response = authEmailService.validateEmailCode(request);
-    return ResponseUtil.success(AuthSuccessCode.SUCCESS_VALIDATE_EMAIL_CODE, response);
+    return ResponseUtil.success(SUCCESS_VALIDATE_EMAIL_CODE, response);
   }
 
   @Override
