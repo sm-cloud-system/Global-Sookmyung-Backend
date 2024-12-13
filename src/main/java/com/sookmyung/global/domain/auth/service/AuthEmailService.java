0478ae188722e5c1112248990cfc6a624bea9ce6
sung-silver <jinna0319@gmail.com>
2024-12-13 11:01:35 +0900

    [FEAT] 이메일 인증 서비스 클래스 분리 및 이메일 전송 로직 반영

--- src/main/java/com/sookmyung/global/domain/auth/service/AuthEmailService.java
+++ src/main/java/com/sookmyung/global/domain/auth/service/AuthEmailService.java
@@ -3,7 +3,7 @@ package com.sookmyung.global.domain.auth.service;
 import com.sookmyung.global.domain.auth.dto.request.*;
 import com.sookmyung.global.domain.auth.dto.response.*;
 
-public interface AuthService {
+public interface AuthEmailService {
   void createEmailCode(IssueEmailCodeRequest request);
 
   IssueTokenForGuestResponse validateEmailCode(EmailVerificationRequest request);
