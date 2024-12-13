a7a5842d6938c3687e3742517a7abb15c3ee173a
sung-silver <jinna0319@gmail.com>
2024-12-13 12:17:02 +0900

    [FEAT] 이메일 인증 기능 관련 성공 코드 정의

---
+++ src/main/java/com/sookmyung/global/domain/auth/code/AuthSuccessCode.java
@@ -0,0 +1,27 @@
+package com.sookmyung.global.domain.auth.code;
+
+import org.springframework.http.*;
+
+import com.sookmyung.global.common.code.*;
+
+import lombok.*;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum AuthSuccessCode implements SuccessCode {
+  SUCCESS_CREATE_AND_SEND_EMAIL_CODE(HttpStatus.OK, "이메일 인증코드 생성 및 전송에 성공했습니다"),
+  SUCCESS_VALIDATE_EMAIL_CODE(HttpStatus.OK, "이메일 인증에 성공했습니다");
+
+  private final HttpStatus status;
+  private final String message;
+
+  @Override
+  public HttpStatus status() {
+    return this.status;
+  }
+
+  @Override
+  public String message() {
+    return this.message;
+  }
+}
