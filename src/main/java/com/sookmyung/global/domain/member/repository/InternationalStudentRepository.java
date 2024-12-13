10488e2568943d844dd8836941c498dce3de212c
sung-silver <jinna0319@gmail.com>
2024-12-13 15:25:59 +0900

    [FEAT] InternationalStudent 서비스에 생성 로직 구현

---
+++ src/main/java/com/sookmyung/global/domain/member/repository/InternationalStudentRepository.java
@@ -0,0 +1,7 @@
+package com.sookmyung.global.domain.member.repository;
+
+import org.springframework.data.jpa.repository.*;
+
+import com.sookmyung.global.domain.member.entity.*;
+
+public interface InternationalStudentRepository extends JpaRepository<InternationalStudent, Long> {}
