61124ea80f81391a0d34aa2b67c94b9ecc5d845b
sung-silver <jinna0319@gmail.com>
2024-12-14 20:39:26 +0900

    [FEAT] 닉네임 검증 API 구현

--- src/main/java/com/sookmyung/global/domain/member/controller/MemberController.java
+++ src/main/java/com/sookmyung/global/domain/member/controller/MemberController.java
@@ -1,9 +1,14 @@
 package com.sookmyung.global.domain.member.controller;
 
+import static com.sookmyung.global.common.code.success.MemberSuccessCode.SUCCESS_VALIDATE_NICKNAME;
+
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import com.sookmyung.global.common.response.*;
+import com.sookmyung.global.common.util.*;
+import com.sookmyung.global.domain.member.dto.response.*;
+import com.sookmyung.global.domain.member.service.*;
 
 import lombok.*;
 
@@ -11,11 +16,13 @@ import lombok.*;
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController implements MemberApi {
+  private final MemberService memberService;
 
   @GetMapping("/validate-nickname")
   @Override
   public ResponseEntity<ResponseTemplate<?>> validateNickname(
-      @RequestParam("name") final String name) {
-    return null;
+      @RequestParam("nickname") final String nickname) {
+    ValidateNicknameResponse response = memberService.validateNickname(nickname);
+    return ResponseUtil.success(SUCCESS_VALIDATE_NICKNAME, response);
   }
 }
