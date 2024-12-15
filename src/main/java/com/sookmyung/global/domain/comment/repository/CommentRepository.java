5611041a5f0aafb4f3a9d4bca561013229ef1798
sung-silver <jinna0319@gmail.com>
2024-12-15 22:50:11 +0900

    [FEAT] 댓글 리스트 조회 서비스 기능 구현

--- src/main/java/com/sookmyung/global/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sookmyung/global/domain/comment/repository/CommentRepository.java
@@ -1,5 +1,7 @@
 package com.sookmyung.global.domain.comment.repository;
 
+import java.util.*;
+
 import org.springframework.data.jpa.repository.*;
 
 import com.sookmyung.global.domain.comment.entity.*;
@@ -7,4 +9,6 @@ import com.sookmyung.global.domain.post.entity.*;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
   int countByPost(Post post);
+
+  List<Comment> findAllByPost(Post post);
 }
--- src/main/java/com/sookmyung/global/domain/comment/service/CommentService.java
+++ src/main/java/com/sookmyung/global/domain/comment/service/CommentService.java
@@ -1,7 +1,12 @@
 package com.sookmyung.global.domain.comment.service;
 
+import java.util.*;
+
 import com.sookmyung.global.domain.comment.dto.request.*;
+import com.sookmyung.global.domain.comment.dto.response.*;
 
 public interface CommentService {
   Long createComment(Long memberId, Long postId, CreateCommentRequest request);
+
+  List<CommentResponse> getComments(Long memberId, Long postId);
 }
