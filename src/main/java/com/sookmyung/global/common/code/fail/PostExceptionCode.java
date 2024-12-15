0ad34792e11b8c9cb66c4936bf3a830eef284b9b
sung-silver <jinna0319@gmail.com>
2024-12-15 15:16:08 +0900

    [FEAT] 게시글 생성 요청에 대한 예외 구현

---
+++ src/main/java/com/sookmyung/global/common/code/fail/PostExceptionCode.java
@@ -0,0 +1,24 @@
+package com.sookmyung.global.common.code.fail;
+
+import org.springframework.http.*;
+
+import lombok.*;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum PostExceptionCode implements ExceptionCode {
+  INVALID_CREATE_POST_REQUEST(HttpStatus.BAD_REQUEST, "올바르지 않은 게시글 생성 요청입니다");
+
+  private final HttpStatus status;
+  private final String message;
+
+  @Override
+  public HttpStatus status() {
+    return this.status;
+  }
+
+  @Override
+  public String message() {
+    return this.message;
+  }
+}
