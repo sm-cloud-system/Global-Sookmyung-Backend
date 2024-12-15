97da34a98ed972d6663e8eddcad4a8f8965aedc2
sung-silver <jinna0319@gmail.com>
2024-12-15 18:13:45 +0900

    [FEAT] 게시글 작성 일자를 PostResponse에서 반환하도록 변경

--- src/main/java/com/sookmyung/global/domain/post/dto/response/PostResponse.java
+++ src/main/java/com/sookmyung/global/domain/post/dto/response/PostResponse.java
@@ -1,11 +1,14 @@
 package com.sookmyung.global.domain.post.dto.response;
 
+import java.time.format.*;
+
 import com.sookmyung.global.domain.post.entity.*;
 
 public record PostResponse(
     Boolean isMyPost,
     Long postId,
     String authorName,
+    String createdAt,
     String title,
     String content,
     Boolean isLiked,
@@ -25,6 +28,7 @@ public record PostResponse(
         post.getAuthor().getNickname(),
         post.getTitle(),
         post.getContent(),
+        post.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy.MM.dd")),
         isLiked,
         likeCount,
         isBookmarked,
