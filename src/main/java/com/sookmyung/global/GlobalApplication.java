0ae80639f67254c30b59eaf5473c5291a43a48ca
sung-silver <jinna0319@gmail.com>
2024-12-15 15:52:00 +0900

    [FEAT] EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/sookmyung/global/GlobalApplication.java
+++ src/main/java/com/sookmyung/global/GlobalApplication.java
@@ -2,8 +2,10 @@ package com.sookmyung.global;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.*;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GlobalApplication {
 
   public static void main(String[] args) {
