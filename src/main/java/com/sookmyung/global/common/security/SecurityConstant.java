37d146204cc780bb8903e415ee45b3173deea937
sung-silver <jinna0319@gmail.com>
2024-12-15 14:49:27 +0900

    [CHORE] 로그인 경로 AUTH_WHITELIST에 추가

--- src/main/java/com/sookmyung/global/common/security/SecurityConstant.java
+++ src/main/java/com/sookmyung/global/common/security/SecurityConstant.java
@@ -2,11 +2,8 @@ package com.sookmyung.global.common.security;
 
 public final class SecurityConstant {
   public static final String[] AUTH_WHITELIST = {
-    "/",
-    "/error",
-    "/favicon.ico",
-    "/auth/issue/code",
-    "/auth/verify/email",
+    "/", "/error", "/favicon.ico",
+    "/auth/issue/code", "/auth/verify/email", "/auth/login",
     "/member/validate-nickname"
   };
   public static final String[] AUTH_WHITELIST_WILDCARD = {
