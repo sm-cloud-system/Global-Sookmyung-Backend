53775c4c4c2fb7c64a8017d45452bf540972c675
sung-silver <jinna0319@gmail.com>
2024-12-15 15:53:03 +0900

    [FEAT] PostRepository에 findByIdOrThrow 구현

--- src/main/java/com/sookmyung/global/common/code/fail/PostExceptionCode.java
+++ src/main/java/com/sookmyung/global/common/code/fail/PostExceptionCode.java
@@ -7,7 +7,8 @@ import lombok.*;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum PostExceptionCode implements ExceptionCode {
-  INVALID_CREATE_POST_REQUEST(HttpStatus.BAD_REQUEST, "올바르지 않은 게시글 생성 요청입니다");
+  INVALID_CREATE_POST_REQUEST(HttpStatus.BAD_REQUEST, "올바르지 않은 게시글 생성 요청입니다"),
+  NOT_FOUND_POST(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다");
 
   private final HttpStatus status;
   private final String message;
