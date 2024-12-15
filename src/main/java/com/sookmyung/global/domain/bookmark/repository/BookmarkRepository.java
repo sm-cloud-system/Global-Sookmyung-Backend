956e881b1de1c84b5233e2027919f6d7f0e46250
sung-silver <jinna0319@gmail.com>
2024-12-15 16:12:49 +0900

    [FEAT] BookMark 레포지토리 정의

---
+++ src/main/java/com/sookmyung/global/domain/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,12 @@
+package com.sookmyung.global.domain.bookmark.repository;
+
+import org.springframework.data.jpa.repository.*;
+
+import com.sookmyung.global.domain.bookmark.entity.*;
+import com.sookmyung.global.domain.like.entity.*;
+import com.sookmyung.global.domain.member.entity.*;
+import com.sookmyung.global.domain.post.entity.*;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+  void deleteByMemberAndPost(Member member, Post post);
+}
