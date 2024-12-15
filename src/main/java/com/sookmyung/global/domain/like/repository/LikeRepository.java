444cf26c6e0e29b5467ac26f9c77306180971856
sung-silver <jinna0319@gmail.com>
2024-12-15 15:46:19 +0900

    [FEAT] LikeRepository 구현

---
+++ src/main/java/com/sookmyung/global/domain/like/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.sookmyung.global.domain.like.repository;
+
+import org.springframework.data.jpa.repository.*;
+
+import com.sookmyung.global.domain.like.entity.*;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {}
