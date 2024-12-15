7fa98c1c15874c224585df62f755fe75fc04ed12
sung-silver <jinna0319@gmail.com>
2024-12-15 15:53:03 +0900

    [FEAT] PostRepository에 findByIdOrThrow 구현

--- src/main/java/com/sookmyung/global/domain/post/repository/PostRepository.java
+++ src/main/java/com/sookmyung/global/domain/post/repository/PostRepository.java
@@ -1,7 +1,14 @@
 package com.sookmyung.global.domain.post.repository;
 
+import static com.sookmyung.global.common.code.fail.PostExceptionCode.NOT_FOUND_POST;
+
 import org.springframework.data.jpa.repository.*;
 
+import com.sookmyung.global.common.exception.*;
 import com.sookmyung.global.domain.post.entity.*;
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+public interface PostRepository extends JpaRepository<Post, Long> {
+  default Post findByIdOrThrow(Long postId) {
+    return findById(postId).orElseThrow(() -> new PostException(NOT_FOUND_POST));
+  }
+}
