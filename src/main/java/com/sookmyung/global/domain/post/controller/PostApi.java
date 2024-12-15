2fbaee1d08775b9eda88ef8f71820916c588e35d
sung-silver <jinna0319@gmail.com>
2024-12-15 15:26:47 +0900

    [FEAT] 게시글 생성 API 구현

--- src/main/java/com/sookmyung/global/domain/post/controller/PostApi.java
+++ src/main/java/com/sookmyung/global/domain/post/controller/PostApi.java
@@ -1,10 +1,12 @@
 package com.sookmyung.global.domain.post.controller;
 
+import jakarta.validation.*;
+
 import org.springframework.http.*;
 
 import com.sookmyung.global.common.response.*;
 import com.sookmyung.global.domain.post.dto.*;
 
 public interface PostApi {
-  ResponseEntity<ResponseTemplate> createPost(Long memberId, CreatePostRequest request);
+  ResponseEntity<ResponseTemplate> createPost(Long memberId, @Valid CreatePostRequest request);
 }
--- src/main/java/com/sookmyung/global/domain/post/controller/PostController.java
+++ src/main/java/com/sookmyung/global/domain/post/controller/PostController.java
@@ -1,11 +1,20 @@
 package com.sookmyung.global.domain.post.controller;
 
+import static com.sookmyung.global.common.code.success.PostSuccessCode.SUCCESS_CREATE_POST;
+
+import java.net.*;
+
+import jakarta.validation.*;
+
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.*;
 
 import com.sookmyung.global.common.response.*;
 import com.sookmyung.global.common.security.*;
+import com.sookmyung.global.common.util.*;
 import com.sookmyung.global.domain.post.dto.*;
+import com.sookmyung.global.domain.post.service.*;
 
 import lombok.*;
 
@@ -13,11 +22,23 @@ import lombok.*;
 @RestController
 @RequiredArgsConstructor
 public class PostController implements PostApi {
+  private static final String POST_PATH = "/posts/";
+  private final PostService postService;
 
   @PostMapping
   @Override
   public ResponseEntity<ResponseTemplate> createPost(
-      @AuthMember Long memberId, CreatePostRequest request) {
-    return null;
+      @AuthMember Long memberId, @RequestBody @Valid CreatePostRequest request) {
+    Long postId = postService.createPost(memberId, request);
+    URI location =
+        ServletUriComponentsBuilder.fromCurrentRequest()
+            .path(POST_PATH)
+            .path("{id}")
+            .buildAndExpand(postId)
+            .toUri();
+    HttpHeaders headers = new HttpHeaders();
+
+    headers.setLocation(location);
+    return ResponseUtil.success(SUCCESS_CREATE_POST, headers);
   }
 }
