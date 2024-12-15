e685c72a1531d9877e48a0f410528c962ad68e27
sung-silver <jinna0319@gmail.com>
2024-12-15 20:31:48 +0900

    [FEAT] PostType에 게시판명을 명시적으로 전달할 수 있도록 board로 필드 이름 변경

--- src/main/java/com/sookmyung/global/common/enums/PostType.java
+++ src/main/java/com/sookmyung/global/common/enums/PostType.java
@@ -3,9 +3,10 @@ package com.sookmyung.global.common.enums;
 import lombok.*;
 
 @RequiredArgsConstructor
+@Getter
 public enum PostType {
   ALL_STUDENT("All Students"),
   INTERNATIONAL_STUDENT("International Students");
 
-  private final String name;
+  private final String board;
 }
