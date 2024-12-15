522fdba725023be00669ad1df061116878ec1338
sung-silver <jinna0319@gmail.com>
2024-12-15 14:49:04 +0900

    [FEAT] 프로필 조회 API 구현

--- src/main/java/com/sookmyung/global/common/code/success/MemberSuccessCode.java
+++ src/main/java/com/sookmyung/global/common/code/success/MemberSuccessCode.java
@@ -7,7 +7,8 @@ import lombok.*;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum MemberSuccessCode implements SuccessCode {
-  SUCCESS_VALIDATE_NICKNAME(HttpStatus.OK, "닉네임 검증에 성공했습니다");
+  SUCCESS_VALIDATE_NICKNAME(HttpStatus.OK, "닉네임 검증에 성공했습니다"),
+  SUCCESS_GET_PROFILE(HttpStatus.OK, "프로필 조회에 성공했습니다");
 
   private final HttpStatus status;
   private final String message;
